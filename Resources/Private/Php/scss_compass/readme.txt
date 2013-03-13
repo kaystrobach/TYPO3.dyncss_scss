54bceed45e389a11a03e896735628c4261f9722b
Kay Strobach <github@kay-strobach.de>
2013-03-13 21:15:50 +0100

    refactoring

--- Resources/Private/Php/scss_compass/readme.txt
+++
@@ -1,1 +0,0 @@
-https://github.com/leafo/scssphp-compass
--- ext_localconf.php
+++ ext_localconf.php
@@ -1,13 +1,3 @@
 <?php
 
-$scssParserConfig = unserialize($TYPO3_CONF_VARS['EXT']['extConf'][$_EXTKEY]);
-
-if((!array_key_exists('parser', $scssParserConfig)) || ($scssParserConfig['parser'] === 'php')) {
-	tx_DynCss_Configuration_BeRegistry::get()->registerFileHandler('scss', 'tx_DyncssScss_Parser');
-} elseif($scssParserConfig['parser'] === 'compass') {
-	tx_DynCss_Configuration_BeRegistry::get()->registerFileHandler('scss', 'tx_DyncssScss_ParserCompass');
-} else {
-	// mmhm misconfiguration, no parser at all ;)
-}
-
-unset($scssParserConfig);
+tx_DynCss_Configuration_BeRegistry::get()->registerFileHandler('scss', 'tx_DyncssScss_Parser');
