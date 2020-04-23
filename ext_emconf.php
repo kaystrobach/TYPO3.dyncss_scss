0da8c37f086d13eb9ff07f436142b60bbc0e4483
Thomas Deuling <1298841+tdeuling@users.noreply.github.com>
2020-04-23 15:53:42 +0200

    [TASK] Revert changes

--- ext_emconf.php
+++ ext_emconf.php
@@ -10,11 +10,11 @@
 # writing. "version" and "dependencies" must not be touched!
 ########################################################################
 
-$EM_CONF['dyncss_scss'] = [
-    'title' => 'DynCss - SCSS Parser (based on Leafo SCSS-Parser v1.0.8)',
+$EM_CONF[$_EXTKEY] = array(
+    'title' => 'DynCss - SCSS Parser (based on Leafo SCSS-Parser v0.7.5)',
     'description' => 'DynCss-Parser',
     'category' => 'fe',
-    'version' => '2.0.0',
+    'version' => '1.2.0',
     'state' => 'stable',
     'uploadfolder' => 0,
     'createDirs' => '',
@@ -24,15 +24,15 @@ $EM_CONF['dyncss_scss'] = [
     'author_company' => '',
     'CGLcompliance' => '',
     'CGLcompliance_note' => '',
-    'constraints' => [
-        'depends' => [
-            'dyncss' => '1.0.0-1.99.99',
-            'typo3' => '9.5.0-10.4.99'
-        ],
-        'conflicts' => [
-        ],
-        'suggests' => [
-        ],
-    ],
+    'constraints' => array(
+        'depends' => array(
+            'dyncss' => '0.8.0-0.8.99',
+            'typo3'  => '7.6.15-8.7.99'
+        ),
+        'conflicts' => array(
+        ),
+        'suggests' => array(
+        ),
+    ),
     '_md5_values_when_last_written' => 'a:11:{s:9:"ChangeLog";s:4:"dfaa";s:10:"README.txt";s:4:"ee2d";s:21:"ext_conf_template.txt";s:4:"1cbe";s:12:"ext_icon.gif";s:4:"6c2c";s:17:"ext_localconf.php";s:4:"e47e";s:28:"ext_typoscript_constants.txt";s:4:"f81f";s:24:"ext_typoscript_setup.txt";s:4:"e8b6";s:13:"locallang.xml";s:4:"13b4";s:23:"lib/lessc-0.2.0.inc.php";s:4:"efef";s:23:"lib/lessc-0.3.3.inc.php";s:4:"e18e";s:25:"pi1/class.tx_less_pi1.php";s:4:"6db7";}',
-];
+);
